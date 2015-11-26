0de659286c745f99bc0e6f1f5361940f6c557ec1
billthefighter <billthefighter@gmail.com>
2015-11-25 17:20:27 -0800

    Made a typo.

--- sunrise.py
+++ sunrise.py
@@ -16,7 +16,8 @@ height         = 32  # types (incl. tiling).  Other code may need tweaks.
 # ------------Image Block---------------------
 image       = Image.new('RGB', (64, 32))
 draw        = ImageDraw.Draw(image)
-registerows = 32
+
+rows = 32
 chains = 2
 parallel = 1
 matrix = RGBMatrix(rows, chains, parallel)
