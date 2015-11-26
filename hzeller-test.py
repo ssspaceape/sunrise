c010defe209698a090ad270efeacf72ab2c59946
billthefighter <billthefighter@gmail.com>
2015-11-25 16:30:01 -0800

    correct unexpected indent

--- hzeller-test.py
+++ hzeller-test.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
-      from rgbmatrix import RGBMatrix
-      import time
+from rgbmatrix import RGBMatrix
+import time
 
-      rows = 16
-      chains = 1
-      parallel = 2
-      myMatrix = RGBMatrix(rows, chains, parallel)
-      myMatrix.Fill(255, 0, 0)
-      time.sleep(5)
-      myMatrix.Clear()
+rows = 16
+chains = 1
+parallel = 2
+myMatrix = RGBMatrix(rows, chains, parallel)
+myMatrix.Fill(255, 0, 0)
+time.sleep(5)
+myMatrix.Clear()
