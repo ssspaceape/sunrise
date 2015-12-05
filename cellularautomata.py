3e4e303d4ae0f9d422655663a67c7a043dab40e5
ssspaceape <jared.j.huntington@gmail.com>
2015-12-05 07:45:24 -0800

    it totslly eork dogggg
    suppopopadow

--- cellularautomata.py
+++ cellularautomata.py
@@ -1,60 +1,96 @@
+#! python2.7
 from PIL import Image
 from PIL import ImageOps
 import random
+import sys
 #from rgbmatrix import Adafruit_RGBmatrix
 import atexit
 import time
+from samplebase import SampleBase
 #define inputs
 runrule = 3
-
-#matrix = Adafruit_RGBmatrix(32, 1)
-
-#def clearOnExit():
-#    matrix.Clear()
+#RGB on and off color values
+onColor = [0,0,100]
+offColor = [0,255,0]
 
 #atexit.register(clearOnExit)
+class CellularAutomata(SampleBase):
+	def __init__(self, *args, **kwargs):
+		super(CellularAutomata, self).__init__(*args, **kwargs)
 
-def step(a, rule, k=2, r=1):
-    nbrs = [a[c:] + a[:c] for c in range(-r, r+1, 1)]
-    l = []
-    for t in apply(zip, nbrs):
-        result = 0
-        for i in t:
-            result = (result * k) + i
-        l.append(result)
-    return [((rule / (k ** v)) % k) for v in l]
+	def step(self,a, rule, k=2, r=1):
+		nbrs = [a[c:] + a[:c] for c in range(-r, r+1, 1)]
+		l = []
+		for t in apply(zip, nbrs):
+			result = 0
+			for i in t:
+				result = (result * k) + i
+			l.append(result)
+		return [((rule / (k ** v)) % k) for v in l]
 
-def basicRun(rule, steps, stepper, seed=[1], k=2, r=1):
-    print steps
-    seed=[1]
-    for x in xrange(0,steps):
-        seed.append(random.randint(0,1))
-        pass
-    #seed = ([0] * steps) + seed + ([0] * steps)
-    #print seed
-    result = seed[:]
-    for i in range(steps):#----------------------------------------Change this For to a while loop, which will keep it looping until interrupt
-        seed = stepper(seed, rule, k=k, r=r)
-        result += seed[:]
-        #----------------------------------------At this point, I want to append 
-    return result, (len(seed), steps + 1)
+	def basicRun(self,rule, steps, seed=[1], k=2, r=1):
+		#print(steps)
+		seed=[1]
+		for x in range(0,steps):
+			seed.append(random.randint(0,1))
+			pass
+		#seed = ([0] * steps) + seed + ([0] * steps)
+		#print seed
+		result = seed[:]
+		for i in range(steps):#----------------------------------------Change this For to a while loop, which will keep it looping until interrupt
+			seed = self.step(seed, rule, k=k, r=r)
+			result += seed[:]
+			#----------------------------------------At this point, I want to append 
+		#print result
+		return result, (len(seed), steps + 1)
 
-def showResult(result, dims, k=2):
-    i = Image.new("L", dims)
-    i.putdata(result, (255 / (k - 1)))
-    i = i.crop(i.getbbox())
-    i = ImageOps.invert(i)
-    i.load()
-    i.show()
-    #matrix.SetImage(i.im.id, 0, 0)
-    time.sleep(2)
+	def nextRun(self,rule, steps, matrix, seed=[1], k=2, r=1):
+		for i in range(steps+1):
+			matrix.pop(0)
+		seed = self.step(matrix[0:steps+1], rule, k=k, r=r)
+		matrix += seed[:]
+		#print result
+		return matrix, (len(seed), steps + 1)
+		
+	def drawLEDs(self,matrix, dimensions,canvas):
+		dimensions=dimensions+1
+		#this is some code for printing out matrix on the command line
+		for y in range(dimensions):
+			for x in range(dimensions):
+				
+				if matrix[y * (dimensions) + x] == 1:
+				#	sys.stdout.write('X')
+					canvas.SetPixel(y, x, onColor[0], onColor[1], onColor[2])
+				else:
+				#	sys.stdout.write(' ')
+					canvas.SetPixel(y, x, offColor[0], offColor[1], offColor[2])
+				#print matrix[y * (dimensions) + x],
+			print " "
+		print " "
+		offsetCanvas = self.matrix.SwapOnVSync(canvas)
+		#for y in range(dimensions):
+		#	for x in range(dimensions):
+		#		print x," ",y," ",y * (dimensions) + x," ",matrix[y * (dimensions) + x]
+		#	print " "
+	def Run(self):
+		runrule=30
+		lines=32
+		print "run has been called"
+		offsetCanvas = [1]
+		offsetCanvas = self.matrix.CreateFrameCanvas()
 
-def runTest(runrule,lines):
-    result, dims = basicRun(runrule, lines, step)
-    showResult(result, dims)
+		result, dims = self.basicRun(runrule, lines)
+		self.drawLEDs(result, lines, offsetCanvas)
+		while 1:
+			self.nextRun(runrule, lines, result)
+			#showResult(result, dims)
+			self.drawLEDs(result, lines,offsetCanvas)
+			time.sleep(0.015)
 
 if __name__ == "__main__":
-    runTest(30,200)
-    #for x in [30,90,54,110]:
-        #runTest(x,32)
-        #pass
+    parser = CellularAutomata()
+    if (not parser.process()):
+        parser.print_help()
+	#for x in [30,90,54,110]:
+	#runTest(x,32)
+	#pass
